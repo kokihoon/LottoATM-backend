e5c23ca9b4fd78dcf0bc38f1b63c2951df880653
kokihoon <rhrlgns777@gmail.com>
2020-04-17 23:19:36 +0900

    modified AtmApplication

--- src/main/java/com/lottoatm/atm/AtmApplication.java
+++ src/main/java/com/lottoatm/atm/AtmApplication.java
@@ -2,17 +2,24 @@ package com.lottoatm.atm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-@RestController
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+        AtmApplication.class,
+        Jsr310JpaConverters.class
+})
 public class AtmApplication {
 
-    @GetMapping("/hi")
-    public String hi() {
-        return "hi";
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
+
     public static void main(String[] args) {
         SpringApplication.run(AtmApplication.class, args);
     }
