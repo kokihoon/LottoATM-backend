0cb31b9ecdf3b41e2087c22b6e283bf645010e64
kokihoon <rhrlgns777@gmail.com>
2020-04-17 23:17:47 +0900

    AppException

--- src/main/java/com/lottoatm/atm/exception/AppException.java
+++ src/main/java/com/lottoatm/atm/exception/AppException.java
@@ -1,4 +1,15 @@
 package com.lottoatm.atm.exception;
 
-public class AppException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
