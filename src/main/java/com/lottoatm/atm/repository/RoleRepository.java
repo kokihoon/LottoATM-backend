a0ef0be5adb3d95db6532e6d2e006fa50eb7ac70
kokihoon <rhrlgns777@gmail.com>
2020-04-17 23:16:48 +0900

    User, Role Repository

--- src/main/java/com/lottoatm/atm/repository/RoleRepository.java
+++ src/main/java/com/lottoatm/atm/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.lottoatm.atm.repository;
 
-public class RoleRepository {
+import com.lottoatm.atm.model.Role;
+import com.lottoatm.atm.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
 }
