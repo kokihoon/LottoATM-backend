3bd22b3edb5a9c02b2c8430798057f7b004ddf03
kokihoon <rhrlgns777@gmail.com>
2020-05-12 01:24:32 +0900

    change username

--- src/main/java/com/lottoatm/atm/controller/AuthController.java
+++ src/main/java/com/lottoatm/atm/controller/AuthController.java
@@ -51,7 +51,7 @@ public class AuthController {
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getUsername(),
                         loginRequest.getPassword()
                 )
         );
